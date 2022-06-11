920200c907cafa8d85d24a4c599112993bb21ecd
GGshkaa <ggizibrizi@mail.ru>
2022-06-11 19:09:37 +0300

    validate windows

--- src/js/script.js
+++ src/js/script.js
@@ -56,5 +56,35 @@ $(document).ready(function(){
                 $('.overlay, #order').fadeIn('slow')
             });
         });
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },
+                phone: "required",
+                email: {
+                   required: true,
+                   email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Введите свой телефон",
+                email: {
+                  required: "Введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+              }
+        }); 
+    };
+
+   validateForms('#consultation-form');
+   validateForms('#consultation form');
+   validateForms('#order form');
 });
 
